28d1edfe37b092baf62a7df30e54e432b9b63d7e
moe-kahala <mohammad.kahala@quizplus.com>
2024-02-01 10:36:00 +0200

    initiating project with remix

---
+++ app/routes/_index.tsx
@@ -0,0 +1,8 @@
+
+export default function Index() {
+  return (
+    <div className='w-screen h-screen flex items-center justify-center text-3xl font-bold'>
+      <h1>Welcome to Ask Gram</h1>
+    </div>
+  );
+}
