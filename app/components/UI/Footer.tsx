55a69507db559f785d8e42218df0be6d635b458e
moe-kahala <mohammad.kahala@quizplus.com>
2024-02-19 09:40:06 +0200

    updated footer

--- app/components/UI/Footer.tsx
+++ app/components/UI/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     <footer className="mt-auto container gap-2 sm:gap-0 text-xs sm:text-base border-t-0.2 border-[#ebf2f6] px-4 md:px-14 py-7 text-[#6e777f] flex justify-center sm:justify-between items-center flex-wrap">
       <p>
         All copyrights are reserved to{" "}
-        <span className="font-bold">{`QUETIONS ® ${currentYear}`}</span>
+        <span className="font-bold">{`Askgram ® ${currentYear}`}</span>
       </p>
       <div className="flex gap-7">
         <p>Help</p>
