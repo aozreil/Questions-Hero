f9013871cfba166bdfedc791bb8e37e65513e044
HinnHabib <habib@quizplus.com>
2024-02-19 12:20:11 +0200

    support multiple builds

--- app/config/dev/enviroment.server.ts
+++ app/config/dev/enviroment.server.ts
@@ -1,5 +1,5 @@
-export const BASE_URL = 'https://askgramdev.work';// isDev  ? 'https://askgramdev.work' : 'https://askgram.work';
+export const BASE_URL = 'https://askgramdev.work';
 export const API_URL = 'https://askgramdev.work';
-export const AWS_SECRET_ID = 'dev/askgram/basicAuth'; //isDev  ? 'dev/askgram/basicAuth' : 'prod/askgram/basicAuth';
-export const USERS_CLUSTER = `${API_URL}/api/users`; //isLocal ? `${BASE_URL}/api/users` : `http://askgram-users:8080`;
-export const CONTENT_CLUSTER = `${API_URL}/api/users`; //isLocal ? `${BASE_URL}/api/content` : `http://askgram-content:8080`;
+export const AWS_SECRET_ID = 'dev/askgram/basicAuth';
+export const USERS_CLUSTER = `http://askgram-users:8080`;
+export const CONTENT_CLUSTER = `http://askgram-content:8080`;
