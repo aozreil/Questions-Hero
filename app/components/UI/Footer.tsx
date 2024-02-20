7ed7ccdc2b0fd330e4c88fc5498884c59ff4c4f7
moe-kahala <mohammad.kahala@quizplus.com>
2024-02-20 12:11:54 +0200

    fixed bottom links and anchor tags

--- app/components/UI/Footer.tsx
+++ app/components/UI/Footer.tsx
@@ -4,17 +4,17 @@ import { TERMS_NAVIGATION_LINKS } from "~/routes/_main.terms.$slug";
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
-    <footer className="mt-auto container gap-2 sm:gap-0 text-xs sm:text-base border-t-0.2 border-[#ebf2f6] px-4 md:px-14 py-7 text-[#6e777f] flex justify-center sm:justify-between items-center flex-wrap">
+    <footer className="mt-auto container gap-2 sm:gap-0 text-xs sm:text-base border-t-0.2 border-[#ebf2f6] px-4 md:px-14 py-7 text-[#6e777f] flex justify-center sm:justify-between items-center gap-y-4 max-lg:flex-col">
       <p>
         All copyrights are reserved to{" "}
         <span className="font-bold">{`Askgram ® ${currentYear}`}</span>
       </p>
-      <div className="flex gap-7">
+      <div className="flex gap-y-1 gap-4 sm:gap-x-7">
         {TERMS_NAVIGATION_LINKS?.map(term =>
           <Link
             key={term.text}
             to={term.link}
-            className='hover:text-[#070707]'
+            className='text-center hover:text-[#070707]'
           >{term.text}</Link>
         )}
       </div>
