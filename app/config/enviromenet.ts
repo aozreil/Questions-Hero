35db40bae08be55e973c6510a9ee9b78f34d919d
HinnHabib <habib@quizplus.com>
2024-02-22 09:20:00 +0200

    Add Google Analytics

--- app/config/enviromenet.ts
+++ app/config/enviromenet.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = 'http://localhost:3000'// isDev  ? 'https://askgramdev.work' : 'https://askgram.work';
+export const GOOGLE_ANALYTICS_KEY = 'G-SMNPKG0RR4';
--- app/config/prod/enviromenet.ts
+++ app/config/prod/enviromenet.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = 'https://askgram.com';
+export const GOOGLE_ANALYTICS_KEY = 'G-8Q7YF18VCG';
