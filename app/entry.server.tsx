4c12e89c6cf2c0bd4354908cac80d9a0bc4889a8
moe-kahala <mohammad.kahala@quizplus.com>
2024-02-27 14:55:13 +0200

    added isBot to scripts

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -12,6 +12,7 @@ import { RemixServer } from "@remix-run/react";
 import { isbot } from "isbot";
 import { renderToPipeableStream } from "react-dom/server";
 import { initiateSecreteManager } from './aws.server';
+import { IsBotProvider } from "~/context/IsBotContext";
 
 initiateSecreteManager();
 const ABORT_DELAY = 5_000;
@@ -100,11 +101,13 @@ function handleBrowserRequest(
   return new Promise((resolve, reject) => {
     let shellRendered = false;
     const { pipe, abort } = renderToPipeableStream(
-      <RemixServer
-        context={remixContext}
-        url={request.url}
-        abortDelay={ABORT_DELAY}
-      />,
+      <IsBotProvider isBot={isbot(request.headers.get("User-Agent") ?? "")}>
+        <RemixServer
+          context={remixContext}
+          url={request.url}
+          abortDelay={ABORT_DELAY}
+        />
+      </IsBotProvider>,
       {
         onShellReady() {
           shellRendered = true;
