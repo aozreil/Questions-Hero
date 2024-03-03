c9b6d857f8f27da33c47acb9176519e978b07f57
moe-kahala <mohammad.kahala@quizplus.com>
2024-03-03 11:33:14 +0200

    Fixed Issues with Google sign in

--- app/hooks/useGoogleOneTab.tsx
+++ app/hooks/useGoogleOneTab.tsx
@@ -1,22 +1,24 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { loginWithGoogle } from "~/apis/userAPI";
 import { GOOGLE_SIGN_IN_CLIENT_ID } from "~/config/enviromenet";
 import { useScript } from 'usehooks-ts';
 import { AuthContextType } from "~/context/AuthProvider";
 
 interface Props {
-  updateState: AuthContextType['updateState'],
-  isLoggedIn: boolean,
+  updateState: AuthContextType['updateState'];
+  isLoggedIn: boolean;
 }
 
 export default function useGoogleOneTab({ updateState, isLoggedIn }: Props) {
   const status = useScript('https://accounts.google.com/gsi/client',  {
     removeOnUnmount: true,
   });
+  const refLoggedIn = useRef(false);
 
   useEffect(() => {
     if (!isLoggedIn && status === 'ready') {
       setTimeout(() => {
+        if (refLoggedIn.current) return;
         google.accounts.id.initialize({
           client_id: GOOGLE_SIGN_IN_CLIENT_ID,
           callback: handleGoogleLogin,
@@ -27,11 +29,20 @@ export default function useGoogleOneTab({ updateState, isLoggedIn }: Props) {
     }
   }, [status, isLoggedIn]);
 
+  useEffect(() => {
+    if (isLoggedIn && status === 'ready') {
+      refLoggedIn.current = true;
+      google.accounts.id.cancel();
+    } else {
+      refLoggedIn.current = false;
+    }
+  }, [isLoggedIn]);
+
   const handleGoogleLogin = useCallback(async (response: any) => {
     try {
       if (response?.credential) {
         const userResponse = await loginWithGoogle(response.credential);
-        if (userResponse?.user_id) { updateState(userResponse) }
+        if (userResponse?.id) { updateState(userResponse) }
       }
     } catch (e) {
       console.log(e);
