adf7926f82669ae0bc009e5efe6f436ee7b92a87
HinnHabib <habib@quizplus.com>
2024-03-13 14:42:50 +0200

    Add strict mode for react

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -5,12 +5,14 @@
  */
 
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition } from "react";
+import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
   hydrateRoot(
     document,
-    <RemixBrowser />
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>
   );
 });
