86150ad15c48f8b2ad066f54cb68253058a6beb1
moe-kahala <mohammad.kahala@quizplus.com>
2024-03-26 14:58:28 +0200

    fixed attachments link

--- app/config/dev/enviroment.server.ts
+++ app/config/dev/enviroment.server.ts
@@ -4,4 +4,4 @@ export const USERS_CLUSTER = `http://askgram-users:8080`;
 export const CONTENT_CLUSTER = `http://askgram-content:8080`;
 export const SEARCH_CLUSTER = "http://askgram-search:8080"
 export const VERSION_DATE = `${new Date().toISOString()}`
-export const ATTACHMENTS_BASE = '`https://attachments.askgramdev.work'
+export const ATTACHMENTS_BASE = 'https://attachments.askgramdev.work'
