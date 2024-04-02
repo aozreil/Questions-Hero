622f9dc71a86f8724a2c8d2399a5788e74a35374
moe-kahala <mohammad.kahala@quizplus.com>
2024-04-02 09:21:40 +0300

    updated recaptcha key

--- app/config/dev/enviromenet.ts
+++ app/config/dev/enviromenet.ts
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://askgramdev.work';
 export const GOOGLE_ANALYTICS_KEY = 'G-SMNPKG0RR4';
 export const ASKGRAM_BASE = 'https://askgramdev.work';
 export const GOOGLE_SIGN_IN_CLIENT_ID = '761946262599-ru0jcosed1hrfcjca25g6fmt7v75dfnr.apps.googleusercontent.com'
-export const RECAPTCHA_PUBLIC_KEY = '6LeBQaIpAAAAAI4MZZySIuWtBNJ_YaOU09bC7HDa';
+export const RECAPTCHA_PUBLIC_KEY = '6LeX-aopAAAAAPy0X1xw3m4hdT0TDvSLyX4aT7xM';
--- app/config/prod/enviromenet.ts
+++ app/config/prod/enviromenet.ts
@@ -2,4 +2,4 @@ export const BASE_URL = 'https://askgram.com';
 export const GOOGLE_ANALYTICS_KEY = 'G-8Q7YF18VCG';
 export const ASKGRAM_BASE = 'https://askgram.com';
 export const GOOGLE_SIGN_IN_CLIENT_ID = '825787308849-gmajn6tmg1sftafdl9d62fqj0jqq61ul.apps.googleusercontent.com';
-export const RECAPTCHA_PUBLIC_KEY = '6LeBQaIpAAAAAI4MZZySIuWtBNJ_YaOU09bC7HDa';
+export const RECAPTCHA_PUBLIC_KEY = '6LeX-aopAAAAAPy0X1xw3m4hdT0TDvSLyX4aT7xM';
