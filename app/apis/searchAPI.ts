6f7048f20cb89a4942ef6ad9e35bdb5b50d50f81
moe-kahala <mohammad.kahala@quizplus.com>
2024-05-29 16:48:01 +0300

    fixed merge conflict

--- app/apis/searchAPI.ts
+++ app/apis/searchAPI.ts
@@ -1,12 +1,30 @@
 import axios, { AxiosRequestConfig } from "axios";
 import { ASKGRAM_BASE } from "~/config/enviromenet";
-import { SearchQuestionResponse } from "~/models/searchModel";
+import { OCRSearchResponseInterface, SearchQuestionResponse } from "~/models/searchModel";
+import { getSearchResultsWithDetails } from "~/apis/searchAPI.service";
 
 export async function searchQuestionsAPI(term: string) {
   const res =  await axios.post<{ data: SearchQuestionResponse[] }>(`${ASKGRAM_BASE}/api/search/askgram/search/questions?size=5`, {
     term,
   });
   return res.data
+}
+
+export async function searchByImage(recaptchaToken: string, config?: AxiosRequestConfig): Promise<OCRSearchResponseInterface> {
+  const res =  await axios.post<OCRSearchResponseInterface>
+    (`${ASKGRAM_BASE}/api/search/askgram/image/search`, {
+      recaptcha_token: recaptchaToken,
+    }, { ...config });
+
+  if (res?.data) {
+    const searchData = await getSearchResultsWithDetails(res?.data, true);
+    return {
+      ...res.data,
+      ...searchData,
+    }
+  }
+
+  return res.data;
 }
 
 export async function getUniversities(config?: AxiosRequestConfig) {
