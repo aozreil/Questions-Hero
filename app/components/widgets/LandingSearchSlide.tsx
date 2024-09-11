d1afc78e9e64db5cc4b4c5fc598cf333f0adc78d
moe-kahala <mohammad.kahala@quizplus.com>
2024-09-11 10:41:57 +0300

    fixed landing subjects width

--- app/components/widgets/LandingSearchSlide.tsx
+++ app/components/widgets/LandingSearchSlide.tsx
@@ -17,7 +17,7 @@ export default function LandingSearchSlide() {
       <BackgroundEffect>
         <ExpandableSearch />
       </BackgroundEffect>
-      <div className={"container grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-9 gap-4 w-[90%] md:w-[50rem] max-w-[50rem] z-30"}>
+      <div className={"container grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-9 gap-4 w-[90%] md:w-[55rem] max-w-[55rem] z-30"}>
         <SubjectLink slug={""} label={"All Subjects"} icon={'/assets/images/topics/all-topics.svg'} />
         {Object.entries(SUBJECTS_MAPPER).map(([id, { label, slug, shortTitle }]) => {
           return <SubjectLink key={slug} slug={slug} label={shortTitle ?? label} icon={`/assets/images/topics/${id}.svg`} />;
