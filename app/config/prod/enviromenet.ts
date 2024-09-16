574e2d59796c7f36bc09f2735cb600cc521be52a
HinnHabib <habib@quizplus.com>
2024-09-16 16:54:24 +0300

    update Askgram ai

--- app/config/prod/enviromenet.ts
+++ app/config/prod/enviromenet.ts
@@ -7,4 +7,4 @@ export const RECAPTCHA_PUBLIC_KEY = "6Ldxp7opAAAAAHRE-rV1N3mOuh7m_BBgNl7ak537";
 export const ATTACHMENTS_BASE = "https://attachments.askgram.com";
 export const AI_ANSWER_ACCEPTED_SCORE = 0.9;
 export const ASK_QUESTION_SIMILAR_SCORE = 0.8;
-export const SMARTLOOK_KEY = "80b079c040e44a33067c83a7427a199f47033bd4";
+export const SMARTLOOK_KEY = "64a350d5c07475af6006460f48ce83de08d80caa";
