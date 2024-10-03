24e3459844b7ad57e2a793e2f63fd7135a55451a
moe-kahala <mohammad.kahala@quizplus.com>
2024-10-03 11:26:22 +0300

    changed prod environment to asklix

--- app/config/prod/enviromenet.ts
+++ app/config/prod/enviromenet.ts
@@ -1,10 +1,10 @@
-export const BASE_URL = "https://askgram.com";
+export const BASE_URL = "https://asklix.com";
 export const GOOGLE_ANALYTICS_KEY = "G-8Q7YF18VCG";
-export const SITE_BASE = "https://askgram.com";
+export const SITE_BASE = "https://asklix.com";
 export const GOOGLE_SIGN_IN_CLIENT_ID =
   "825787308849-gmajn6tmg1sftafdl9d62fqj0jqq61ul.apps.googleusercontent.com";
 export const RECAPTCHA_PUBLIC_KEY = "6Ldxp7opAAAAAHRE-rV1N3mOuh7m_BBgNl7ak537";
-export const ATTACHMENTS_BASE = "https://attachments.askgram.com";
+export const ATTACHMENTS_BASE = "https://attachments.asklix.com";
 export const AI_ANSWER_ACCEPTED_SCORE = 0.9;
 export const ASK_QUESTION_SIMILAR_SCORE = 0.8;
 export const SMARTLOOK_KEY = "64a350d5c07475af6006460f48ce83de08d80caa";
