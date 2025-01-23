ffc307e5a3b282137d7b056a229bdb217f49ab20
aozreil <93866815+aozreil@users.noreply.github.com>
2025-01-23 09:35:54 +0200

    testing

--- app/config/prod/enviromenet.ts
+++ app/config/prod/enviromenet.ts
@@ -4,7 +4,7 @@ export const SITE_BASE = "https://questions-hero.com";
 export const GOOGLE_SIGN_IN_CLIENT_ID =
   "113980774638-limh1jo7bmghblqon3qfvfi1hsvp27ft.apps.googleusercontent.com";
 export const RECAPTCHA_PUBLIC_KEY = "6Lcr4lkqAAAAAGmigrjVL1ijobxrf1BTKCStkGyu";
-export const ATTACHMENTS_BASE = "https://attachments.asklix.com";
+export const ATTACHMENTS_BASE = "https://attachments.questions-hero.com";
 export const AI_ANSWER_ACCEPTED_SCORE = 0.9;
 export const ASK_QUESTION_SIMILAR_SCORE = 0.8;
 export const SMARTLOOK_KEY = "9abda499bf4199d20e5e1b4df3f33340794c5e40";
